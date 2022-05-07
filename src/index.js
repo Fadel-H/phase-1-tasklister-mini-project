101f6844735fb465e42231b7c0f85931a3f32dd8
Fadel <fadel.g.heji@gmail.com>
2022-05-07 07:39:15 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  
+  const task = document.getElementById("tasks")
+  document.querySelector("form").addEventListener('submit', (event) => {
+    event.preventDefault()
+    const inputValue = document.getElementById("new-task-description").value
+    
+    
+    toDoList(inputValue)
+  })
+})
+
+   
+function toDoList(description){
+  let p = document.createElement('p')
+  let button = document.createElement("button")
+  button.textContent = ' x'
+  p.textContent = ` ${description} `
+  p.appendChild(button)
+  tasks.appendChild(p)
+
+  button.addEventListener("click", e => toDoListDelete(e))
+}
+
+function toDoListDelete(event){
+  event.target.parentNode.remove()
+}
